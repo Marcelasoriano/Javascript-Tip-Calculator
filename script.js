3b44347c73f739a19f0bbf54007f73c150164e5f
Glitch (glitch-hello-website) <none>
2022-04-17 21:24:21 +0000

    ⛽️🌌 Checkpoint ./index.html:41360910/4397 ./script.js:41360910/2308

--- script.js
+++ script.js
@@ -1,27 +1,44 @@
-/*
-This is your site JavaScript code - you can add interactivity and carry out processing
-- Initially the JS writes a message to the console, and moves a button you can add from the README
-*/
+$(document).ready( function () {
 
-// Print a message in the browser's dev tools console each time the page loads
-// Use your menus or right-click / control-click and choose "Inspect" > "Console"
-console.log("Hello 🌎");
 
-/* 
-Make the "Click me!" button move when the visitor clicks it:
-- First add the button to the page by following the "Next steps" in the README
-*/
-const btn = document.querySelector("button"); // Get the button from the page
-// Detect clicks on the button
-if (btn) {
-  btn.onclick = function() {
-    // The JS works in conjunction with the 'dipped' code in style.css
-    btn.classList.toggle("dipped");
-  };
-}
+    // VARIABLES
+    // ----------------------------------------------------------
 
-// This is a single line JS comment
-/*
-This is a comment that can span multiple lines 
-- use comments to make your own notes!
-*/
+    var amount, percent, result;
+    var calculator = $('.calculator');
+    var calculatorBill = calculator.find('.calculator__bill');
+    var calculatorTip = calculator.find('.calculator__tip');
+    var calculatorResult = calculator.find('.calculator__result');
+    var tipAmount = calculator.find('.tip-amount');
+
+
+    // INIT BILL
+    // ----------------------------------------------------------
+
+    $(window).on('DOMContentLoaded', function () {
+        tipAmount.text( calculatorTip.val() + '%' );
+        amount = calculatorBill.val() * 1;
+        percent = calculatorTip.val() * 1;
+        result = amount + amount * ( percent / 100 );
+        calculatorResult.text( result.toFixed(2) + '$' );
+    });
+
+
+    // RANGE FUNCTION
+    // ----------------------------------------------------------
+
+    calculatorTip.on('change', function () {
+
+        if ( calculatorBill.val() === '' || isNaN( calculatorBill.val() ) ) {
+            alert('Enter bill amount, please!')
+        } else {
+            amount = calculatorBill.val() * 1;
+        }
+
+        tipAmount.text( calculatorTip.val() + '%' );
+        percent = calculatorTip.val() * 1;
+        result = amount + amount * ( percent / 100 );
+        calculatorResult.text( result.toFixed(2) + '$' );
+    });
+
+});
